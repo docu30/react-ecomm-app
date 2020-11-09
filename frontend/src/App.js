e6a8d1c8a719b4a83c3a7908381e62e832a0f8d6
docu30 <docu29@gmail.com>
2020-11-09 18:31:58 +0200

    created product and rating components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from './data'
+import Product from './components/Product'
 function App() {
 
   return (
@@ -18,46 +19,9 @@ function App() {
           <div className="row center">
             {
               data.products.map((product) => (
-                <div key={product._id} className="card">
-                  <div className="card-heading">
-                    <a href={`/product/${product._id}`}>
-                      <img className="medium" src={product.image} alt={product.name} />
-                    </a>
-                  </div>
-                  <div className="card-body">
-                    <a href="product.html">
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                      <span>
-                        <i className="fa fa-star"></i>
-                      </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
+                <Product key={product._id} product={product} />
               ))
             }
-          
-          
-          
-          
-            
-            
-            
-            
           </div>
         </main>
         <footer className="row center">All rights reserved</footer>
